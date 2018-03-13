1f967820a278414c1d07e04b827ae3ac2f2d9784
vivid <vivid@Sexy>
2018-03-12 22:14:39 -0400

    WORK

--- Sortamaniamain/src/Team3SortCompetition.java
+++ Sortamaniamain/src/Team3SortCompetition.java
@@ -4,62 +4,29 @@ public abstract class Team3SortCompetition extends SortCompetition {
 	public String greeting() {
 		return("We winning this lol");
 	}
+	
+//----------------------------------------------------------------------//	
+	
 	public int challengeOne(int[] arr){
-		private int FindMedian(int[]arr1)
-		{
-			int median = arr1[arr1.length/2];
-			if(arr1.length%2 == 1)
-			{
-				return ((arr1[median]+arr1[median+1])/2);
-			}
-			else return (arr1[median]);
+        for (int i = 1; i < arr.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(arr[j] < arr[j-1]){
+                	int x = arr[j];
+               		arr[j] = arr[j-1];
+            		arr[j-1] = x;
+                }
+            }
+        }
+		int what = (arr.length+1)/2;
+		if(arr.length%2==1){ // if length of newarr is odd
+		        return(arr[(arr.length-1)/2]);
+		}else{
+		        return((((arr[what]+arr[what-1])/2)));
 		}
-		int Challenge_1(int[]arr)
-		{
-			return FindMedian(arr);	
-			
-		}
-		
-		
-		
-		
-		int partition(int arr[], int start, int end)
-	    {
-	        int partivot = arr[end]; 
-	        int x = (start-1);
-	        for (int y=start; y<end; y++)
-	        {
-	            if (arr[y] <= partivot)
-	            {
-	                x++;
-	                int temp = arr[x];
-	                arr[x] = arr[y];
-	                arr[y] = temp;
-	            }
-	        }
-	 
-	  
-	        int temp = arr[x+1];
-	        arr[x+1] = arr[end];
-	        arr[end] = temp;
-	 
-	        return x+1;
-	    }
-	 
-	    void sort(int arr[], int start, int end)
-	    {
-	        if (start < end)
-	        {
-	            int parti = partition(arr, start, end);
-	 
-	 
-	            sort(arr, start, parti-1);
-	            sort(arr, parti+1, end);
-	        }
-	    }
-
-
 	}
+	
+//----------------------------------------------------------------------//	
+	
 	//Jacky Chen 
 	public int challengeTwo(String[] arr, String query) { 
         String[]  mergeResult=mergeSort(arr);
@@ -73,8 +40,60 @@ public abstract class Team3SortCompetition extends SortCompetition {
 		
 		return -1;
 	}
+	public static String[] mergeSort(String[] list1)
+	{
+		//base case
+		if(list1.length==1) {
+			return list1;
+		}
+		
+		//recursive
+		String [] left = Arrays.copyOfRange(list1, 0, list1.length/2);
+		String [] right = Arrays.copyOfRange(list1, list1.length/2, list1.length);
+		return(merge(mergeSort(left),mergeSort(right)));
+	}
 	
-	public int challengeThree(int[][] arr){
+	public static String[] merge(String[]list1,String[]list2)
+	{
+		
+		String empty[] = new String[list1.length + list2.length];
+		int i=0;
+		int li=0;
+		int ri=0;
+		while((li<list1.length)&&(ri<list2.length))
+		{
+			if(list1[li].compareTo(list2[ri])>0)
+			{
+				empty[i]=list2[ri];
+				ri++;
+				i++;
+			}
+			else if(list1[li].compareTo(list2[ri])<=0) 
+			{
+				empty[i]=list1[li];
+				li++;
+				i++;
+			}
+		}
+		while(li<list1.length)
+		{
+			empty[i]=list1[li];
+			li++;
+			i++;
+			
+		}
+		while(ri<list2.length)
+		{
+			empty[i]=list2[ri];
+			ri++;
+			i++;
+		}
+		return empty;
+	}
+	
+// ------------------------------------------------------------------//	
+	
+	public int challengeThree(int[] arr){
 		insertionSort(arr);
 		int what = (arr.length+1)/2;
 		if(arr.length%2==1){ // if length of newarr is odd
@@ -83,6 +102,28 @@ public abstract class Team3SortCompetition extends SortCompetition {
 		        return((((arr[what]+arr[what-1])/2)));
 		}
 	}
+	public static void insertionSort(int[] list1)
+	{
+        for (int i = 1; i < list1.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(list1[j] < list1[j-1]){
+                    xinsertionSort(list1,j,j-1);
+                }
+            }
+        }
+	}
+	
+	public static void xinsertionSort(int[]arr, int index1, int index2)
+	{
+		int temp;
+		temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+//------------------------------------------------------------//	
+	
+	// Vivian Yee
 	public int challengeFour(int[][] arr){
 		int key, j, big; 
 		for(int z = 0; z < arr.length; z++) {
@@ -147,91 +188,31 @@ public abstract class Team3SortCompetition extends SortCompetition {
 		    }
 		}
 	}
-	public static int challengeFive(Comparable[] arr, Comparable query){
-
-	}
-	public static String[] mergeSort(String[] list1)
-	{
-		//base case
-		if(list1.length==1) {
-			return list1;
-		}
-		
-		//recursive
-		String [] left = Arrays.copyOfRange(list1, 0, list1.length/2);
-		String [] right = Arrays.copyOfRange(list1, list1.length/2, list1.length);
-		return(merge(mergeSort(left),mergeSort(right)));
-	}
-	
-	public static String[] merge(String[]list1,String[]list2)
-	{
-		
-		String empty[] = new String[list1.length + list2.length];
-		int i=0;
-		int li=0;
-		int ri=0;
-		while((li<list1.length)&&(ri<list2.length))
-		{
-			if(list1[li].compareTo(list2[ri])>0)
-			{
-				empty[i]=list2[ri];
-				ri++;
-				i++;
-			}
-			else if(list1[li].compareTo(list2[ri])<=0) 
-			{
-				empty[i]=list1[li];
-				li++;
-				i++;
-			}
-		}
-		while(li<list1.length)
-		{
-			empty[i]=list1[li];
-			li++;
-			i++;
-			
-		}
-		while(ri<list2.length)
-		{
-			empty[i]=list2[ri];
-			ri++;
-			i++;
-		}
-		return empty;
-	}
-	
-	public static void insertionSort(int[] list1)
-	{
-		int count = 0; 
-		
-		while (count != 0)
-		{
-			count = 0; 
-			
-			for (int i = 0; i < list1.length - 1; i++)
-			{	
-				if (list1[i] > list1[i + 1])
-				{
-					count++;
-					xinsertionSort(list1, i, i + 1);
-				}
-			}
-		}
-	}
-	
-	public static void xinsertionSort(int[]arr, int index1, int index2)
-	{
-		int temp;
-		temp = arr[index1];
-		arr[index1] = arr[index2];
-		arr[index2] = temp;
-	}
-	
 	public static void SwapInt(int[] arr, int index, int index2) {
 		int x = arr[index];
 		arr[index] = arr[index2];
 		arr[index2] = x;
 	}
 	
+//--------------------------------------------------------------------------//
+	// Vivian Yee
+	public int challengeFive(Comparable[] arr, Comparable query){
+        for (int i = 1; i < arr.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(arr[j].compareTo(arr[j-1])<0){
+                	Comparable x = arr[j];
+               		arr[j] = arr[j-1];
+            		arr[j-1] = x;
+                }
+            }
+        }
+        for(int i = 0; i < arr.length; i++) {
+        	if(query.compareTo(arr[i])==0) {
+        		return i;
+        	}
+        }
+        return -1;
+	}
 }
+
+
